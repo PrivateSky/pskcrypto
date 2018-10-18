02e20e97ec190ed69d7e14df7cf01c35d929ad94
Acer 2 <sava.dumitru.daniel@gmail.com>
2018-10-18 15:49:21 +0300

    removed uidGenerator

--- index.js
+++ index.js
@@ -1,11 +1,7 @@
 const PskCrypto = require("./lib/PskCrypto");
-
 const ssutil = require("./signsensusDS/ssutil");
-
-const uidGenerator = require("./lib/uidGenerator").createUidGenerator(200, 64);
 
 module.exports = PskCrypto;
 
 module.exports.hashValues = ssutil.hashValues;
-module.exports.uidGenerator = uidGenerator;
-module.exports.generateUid = uidGenerator.generateUid;
+
