c8ac1195c0532d112c4390ac6d4e2497573e30af
StefanCosminR <stefancosmin.romanescu@gmail.com>
2019-11-25 16:27:46 +0200

    Deleted psk-archiver

--- index.js
+++ index.js
@@ -5,10 +5,6 @@ module.exports = PskCrypto;
 
 module.exports.hashValues = ssutil.hashValues;
 
-if(typeof(___DISABLE_OBSOLETE_ZIP_ARCHIVER_WAIT_FOR_BARS) === 'undefined'){
-    module.exports.PskArchiver = require("./lib/psk-archiver");
-}
-
 module.exports.DuplexStream = require("./lib/utils/DuplexStream");
 
 module.exports.isStream = require("./lib/utils/isStream");
