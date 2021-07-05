ae83c6e99b08737d20c3a45d471068b0bdbed345
skutner <sava.dumitru.daniel@gmail.com>
2021-07-05 13:52:41 +0300

    updated timingSafeEqual function in order for it to work in browser

--- js-mutual-auth-ecies/crypto/index.js
+++ js-mutual-auth-ecies/crypto/index.js
@@ -7,7 +7,14 @@ const sig = require('./digitalsig')
 const crypto = require('crypto')
 
 module.exports = {
-    timingSafeEqual: crypto.timingSafeEqual,
+    timingSafeEqual: function(a, b){
+        const hashA = crypto.createHash("sha256");
+        const digestA = hashA.update(a).digest("hex");
+
+        const hashB = crypto.createHash("sha256");
+        const digestB = hashB.update(b).digest("hex");
+        return digestA === digestB;
+    },
     getRandomBytes: crypto.randomBytes,
     computeDigitalSignature: sig.computeDigitalSignature,
     verifyDigitalSignature: sig.verifyDigitalSignature,
